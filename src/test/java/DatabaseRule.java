e0cc10ce80360c8cae8b184f8c0dd8e85bdcaf27
Scott Holland <scotthollandjr@gmail.com>
2016-05-13 16:32:21 -0700

    Update rule to include all tables

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -12,7 +12,11 @@ public class DatabaseRule extends ExternalResource {
   protected void after() {
     try(Connection con = DB.sql2o.open()) {
       String deleteBandsQuery = "DELETE FROM bands *;";
+      String deleteVenuesQuery = "DELETE FROM venues *;";
+      String deleteBandsVenuesQuery = "DELETE FROM bands_venues *;";
       con.createQuery(deleteBandsQuery).executeUpdate();
+      con.createQuery(deleteVenuesQuery).executeUpdate();
+      con.createQuery(deleteBandsVenuesQuery).executeUpdate();
     }
   }
 }
